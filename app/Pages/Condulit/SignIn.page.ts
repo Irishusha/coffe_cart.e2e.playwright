6add721f9d870a26f4f40e10cb68c5e0ac368f31
irishatesting <braslavets.irisha@gmail.com>
2025-01-02 12:33:29 +0200

    Refactor the code

--- app/Pages/Condulit/SignIn.page.ts
+++ app/Pages/Condulit/SignIn.page.ts
@@ -2,7 +2,6 @@ import { Page } from "@playwright/test";
 
 export class SignInPage {
     constructor(private page: Page) {}
-
     emailField = this.page.locator('//input[@placeholder="Email"]');
     passwordField = this.page.locator('//input[@placeholder = "Password"]');
     signInButton = this.page.locator('//button[contains(@class, "btn-lg")]');
