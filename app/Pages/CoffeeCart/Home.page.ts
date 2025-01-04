aac141587a5a3f9c6853afb7d474584d21e63fbd
irishatesting <braslavets.irisha@gmail.com>
2025-01-04 10:40:31 +0200

    Refactor the code, change the name of variables

--- app/Pages/CoffeeCart/Home.page.ts
+++ app/Pages/CoffeeCart/Home.page.ts
@@ -1,4 +1,4 @@
-import { test, Page, Locator} from '@playwright/test';
+import {Page, Locator} from '@playwright/test';
 
 export async function navigateToHomePage(page: Page) {
     await page.goto('/');
